bd1cc899be7be9e67bb7260658eba8a04391d764
Metabug <metabug@gmail.com>
2024-09-27 12:32:38 +0300

    Made some progress with countdown timer challenge

--- app/pages/countdownTimer/countdownTimerComponent.tsx
+++ app/pages/countdownTimer/countdownTimerComponent.tsx
@@ -1,34 +1,105 @@
-import React, { useState } from "react";
+import React, { useRef, useState, MutableRefObject, useCallback } from "react";
+import moment from "moment";
 
 const CountdownTimerComponent: React.FC = () => {
-    const [timer, setTimer] = useState(0);
+    const [timer, setTimer] = useState(600000);
+    const [timerInput, setTimerInput] = useState("10:00");
     const [isRunning, setIsRunning] = useState(false);
-    const [duration, setDuration] = useState(1000);
+    const intervalId: MutableRefObject<NodeJS.Timeout | null> = useRef(null);
+
+    const setTimerCallback = useCallback(() => {
+        setTimer((prevTimer) => prevTimer - 1000);  
+    }, [])
+
+    /**
+     * Starts the countdown timer.
+     *
+     * Every second, the `timer` state is decremented by 1 second.
+     * When the timer reaches 0, a sound is played and the timer is stopped.
+     */
+    const startTimer = () => {
+        // Set running to true and set timer value
+        console.log(timerInput,timer);
+        setIsRunning(true);
+        const [minute, seconds] = timerInput.split(':');
+        setTimer(moment({minute: parseInt(minute), seconds: parseInt(seconds)}).milliseconds());
+        console.log(timerInput, timer, minute, seconds);
+
+        intervalId.current = setInterval(() => {
+            console.log("Interval callback");
+            setTimerCallback();
+
+            // Play audio when timer reaches zero and stop timer
+            if (timer === 0) {
+                const sound = new Audio("./alarm.mp3");
+                sound.play();
+                stopTimer();
+            }
+        }, 1000);
+    };
+
+    /**
+     * Stops the countdown timer.
+     *
+     * If the timer is currently running, stop it and clear the interval.
+     */
+    const stopTimer = () => {
+        setIsRunning(false);
+        clearInterval(intervalId.current!);
+        setTimerInput(moment.utc(timer).format("mm:ss").toString());
+    };
+
+    const verifyTimerInput = (newInput: string) => {
+        // Make full input with only numbers to correct format
+        if (newInput.match(/^\d{4}$/)) {
+            setTimerInput(
+                `${newInput[0]}${newInput[1]}:${newInput[2]}${newInput[3]}`
+            );
+        }
+
+        // Allow to timer update only with numbers
+        else if (newInput.match(/^\d{0,3}$/)) {
+            setTimerInput(newInput);
+        }
+
+        // Allow to timer update only with numbers with : between them
+        else if (newInput.match(/^\d{0,2}:\d{0,2}$/)) {
+            setTimerInput(newInput);
+        }
+    };
 
     return (
-        <div className="flex flex-col">
-            {/* Timer input field and start/stop button */}
-            <div className="flex flex-col">
-                <label>
-                    <input
-                        type="number"
-                        value={duration}
-                        onChange={(e) => setDuration(parseInt(e.target.value))}
-                    />
-                    <button
-                        onClick={() => {
-                            setIsRunning(!isRunning);
-                            setTimer(duration);
-                        }}
+        <div className="flex flex-col items-center">
+            {
+                /* Switch between input and display text based on isRunning state */
+                isRunning ? (
+                    <div
+                        /* Timer display text*/
+                        className={`flex bg-black-100 w-[25rem] bg-transparent p-4 text-8xl font-bold justify-center outline-none`}
                     >
-                        {isRunning ? "Stop" : "Start"}
-                    </button>
-                </label>
-            </div>
-            {/* Timer clock */}
-            <div className="flex flex-col border-5-[#707070] rounded-lg">
-                <p>{timer}</p>
-            </div>
+                        {moment.utc(timer).format("mm:ss")}
+                    </div>
+                ) : (
+                    /* Timer input field */
+                    <input
+                        className={`flex bg-black-100 w-[25rem] bg-transparent p-4 text-8xl font-bold text-center outline-none`}
+                        type="text"
+                        value={timerInput}
+                        onChange={(e) => verifyTimerInput(e.target.value)}
+                    />
+                )
+            }
+            {/* Start/stop button */}
+            <button
+                className={`w-[10rem] p-4 text-2xl font-bold rounded-lg ${
+                    isRunning ? "bg-red-500" : "bg-green-500"
+                }`}
+                onClick={() => {
+                    isRunning ? stopTimer() : startTimer();
+                }}
+            >
+                {isRunning ? "Stop" : "Start"}
+            </button>
         </div>
     );
 };
